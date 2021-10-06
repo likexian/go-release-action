248a0a424901aa4929ea7c7e27b612b7da7e3623
Li Kexian <i@likexian.com>
2021-10-06 18:17:50 +0800

    add tag push support

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,42 @@
 
 set -ex
 
-TAG_NAME=$(jq -r .release.tag_name $GITHUB_EVENT_PATH | sed -s 's/v//')
-UPLOAD_URL=$(jq -r .release.upload_url $GITHUB_EVENT_PATH | sed -s 's/{?name,label}//')
+TAG_NAME=$(jq -r .release.tag_name $GITHUB_EVENT_PATH)
+if [[ $TAG_NAME != null ]]; then
+    # event.type: release.created
+    UPLOAD_URL=$(jq -r .release.upload_url $GITHUB_EVENT_PATH)
+else
+    # event.type: tags.push
+    IS_CREATED=$(jq -r .created $GITHUB_EVENT_PATH)
+    if [[ $IS_CREATED != true ]]; then
+        echo "Skip: not tag created"
+        exit 0
+    fi
+
+    TAG_NAME=$(jq -r .ref $GITHUB_EVENT_PATH | xargs basename)
+    RELEASE_URL=$(jq -r .repository.releases_url $GITHUB_EVENT_PATH | sed -s 's/{\/id}//')
+
+    curl --data "{\"tag_name\": \"${TAG_NAME}\", \"name\": \"${TAG_NAME}\"}" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -o $GITHUB_EVENT_PATH \
+        $RELEASE_URL
+
+    UPLOAD_URL=$(jq -r .upload_url $GITHUB_EVENT_PATH)
+fi
+
+TAG_NAME=$(echo $TAG_NAME | sed -s 's/v//')
+if [[ $TAG_NAME == null ]]; then
+    echo "Error: TAG_NAME is missing"
+    exit 1
+fi
+
+UPLOAD_URL=$(echo $UPLOAD_URL | sed -s 's/{?name,label}//')
+if [[ $UPLOAD_URL == null ]]; then
+    echo "Error: UPLOAD_URL is missing"
+    exit 1
+fi
 
 if [[ -n $BUILD_DIR ]]; then
     cd $BUILD_DIR
@@ -62,8 +96,6 @@ else
 fi
 
 CHECKSUM=$(sha256sum ${ZIP_FILE_NAME} | awk '{print $1}')
-
-set +x
 
 curl --data-binary "@${ZIP_FILE_NAME}" \
     -H "Content-Type: application/${CONTENT_TYPE}" \
